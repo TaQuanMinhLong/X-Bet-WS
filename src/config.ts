2b659207db34d7e648a6aa7339c6b0bc6b9a0076
TaQuanMinhLong <tqml2013@gmail.com>
2023-08-13 11:07:27 +0700

    init

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const ENV = z.object({
+  CLIENT_ORIGIN: z.string(),
+  REDIS_URL: z.string(),
+});
+
+ENV.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof ENV> {}
+  }
+}
